8c3af11008b9c759080793dfad79888d41c3d5db
mkocik <0de0lv0@gmail.com>
2021-08-11 08:35:10 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Task from "./components/Task";
+
+function App() {
+  return (
+    <div className="App">
+      <InboxScreen />
+    </div>
+  );
+}
+
+export default App;
+//todo storybook of header with some(3) nav buttons +  avatar container with language and dark mode switch
+
